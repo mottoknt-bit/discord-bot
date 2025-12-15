fd65b368aaa4596f355cf4c50942dd63d18e33c2
mottoknt-bit <motto.knt@gmail.com>
2025-12-15 14:08:36 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`ログイン成功: ${client.user.tag}`);
+});
+
+client.on("messageCreate", message => {
+  if (message.author.bot) return;
+  if (message.content === "ping") {
+    message.reply("pong");
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
